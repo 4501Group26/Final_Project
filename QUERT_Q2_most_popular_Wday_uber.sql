56cdb55def17206db22fcde703e9f7e46f3d13e0
JasonZixuanLiu <116607800+JasonZixuanLiu@users.noreply.github.com>
2022-12-09 16:31:51 -0500

    Delete QUERT_Q2_most_popular_Wday_uber.sql
    Delete

--- QUERT_Q2_most_popular_Wday_uber.sql
+++
@@ -1,8 +0,0 @@
-
-SELECT strftime ('%w',substr(pickup_datetime, 1, 19)) AS WeekDay,
-COUNT(strftime ('%w',substr(pickup_datetime, 1, 19)))
-FROM uber_trips
-WHERE pickup_datetime >= '2009-01-01 00:00:00'
-AND pickup_datetime <= '2015-06-30 23:59:59'
-GROUP BY WeekDay
-ORDER BY COUNT(strftime ('%w',substr(pickup_datetime, 1, 19)))
